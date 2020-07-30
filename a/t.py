8fbf043e747b70e1fa9d9434340f6ac4b1ca84fc
Russell Zheng <xz459@cs.drexel.edu>
2020-07-30 16:27:36 -0400

    edit

--- a/t.py
+++ a/t.py
@@ -1,12 +1,14 @@
 import queue
 
 def mazeEnd(maze, path):
+	start = 0
+
 	for row in maze:
 		for pos in row:
 			if pos == 'S':
 				start == row.index(pos)
-
 	
+	return start
 
 if __name__ == '__main__':
 	grid = [['S', ' ', ' ', ' ', ' ', ' '],
@@ -15,4 +17,22 @@ if __name__ == '__main__':
 			[' ', ' ', ' ', '#', ' ', ' '],
 			[' ', ' ', ' ', ' ', '#', ' '],
 			[' ', ' ', ' ', ' ', ' ', 'E']]
-	print(grid)
+
+	# new queue
+	pathQueue = queue.Queue()
+	# start at empty path
+	pathQueue.put("")
+	# placeholder
+	currentPath = ""
+
+	# check to see path leads to exit
+	while not mazeEnd(grid, currentPath):
+		# if not, grab from queue and add UDLR moves
+		currentPath = pathQueue.get()
+		for direction in ["U", "D", "L", "R"]:
+			newPath = currentPath + direction
+			# check validity of newPath, and add to queue
+			if valid(gric, newPath):
+				pathQueue.put(newPath)
+
+	print(mazeEnd)
