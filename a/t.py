cb0b99766b75434697e2d4549e49d4b0b96bfbc6
Russell Zheng <xz459@cs.drexel.edu>
2020-08-01 17:38:09 -0400

    2 lazy but got bfs working

--- a/t.py
+++ a/t.py
@@ -1,38 +1,55 @@
 import queue
 
-def mazeEnd(maze, path):
-	start = 0
-
+def atFinish(maze, path, endCheck, moveCheck):
+	startX = 0
+	startY = 0
+	# find beginning pos or start
 	for row in maze:
 		for pos in row:
 			if pos == 'S':
-				start == row.index(pos)
-	
-	return start
+				startX = row.index(pos)
+				startY = maze.index(row)
+	# finding end pos of path
+	for move in path:
+		if (move == "U"):
+			startY -= 1
+		elif (move == "D"):
+			startY += 1
+		elif (move == "L"):
+			startX -= 1
+		elif (move == "R"):
+			startX += 1
+	# check if end pos == maze end
+	if (endCheck):
+		if (maze[startY][startX] == "E"):
+			print(path)
+			return True
+	# check if move is valid
+	if (moveCheck):
+		if (0 <= startX < len(maze[0]) and 0 <= startY < len(maze)):
+			if (maze[startY][startX] != "#"):
+				return True
+	return False
 
 if __name__ == '__main__':
-	grid = [['S', ' ', ' ', ' ', ' ', ' '],
+	grid = [[' ', ' ', ' ', ' ', 'E', ' '],
 			[' ', '#', ' ', ' ', '#', ' '],
 			[' ', ' ', '#', ' ', '#', ' '],
 			[' ', ' ', ' ', '#', ' ', ' '],
-			[' ', ' ', ' ', ' ', '#', ' '],
-			[' ', ' ', ' ', ' ', ' ', 'E']]
-
+			[' ', 'S', ' ', ' ', '#', ' '],
+			[' ', ' ', ' ', ' ', ' ', ' ']]
 	# new queue
 	pathQueue = queue.Queue()
 	# start at empty path
 	pathQueue.put("")
 	# placeholder
 	currentPath = ""
-
 	# check to see path leads to exit
-	while not mazeEnd(grid, currentPath):
+	while not atFinish(grid, currentPath, True, False):
 		# if not, grab from queue and add UDLR moves
 		currentPath = pathQueue.get()
 		for direction in ["U", "D", "L", "R"]:
 			newPath = currentPath + direction
 			# check validity of newPath, and add to queue
-			if valid(gric, newPath):
+			if atFinish(grid, newPath, False, True):
 				pathQueue.put(newPath)
-
-	print(mazeEnd)
