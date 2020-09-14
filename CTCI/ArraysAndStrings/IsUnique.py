f524c81268f27fb35b6c314809f1a0244739b5ab
Russell Zheng <xz459@cs.drexel.edu>
2020-09-14 17:52:36 -0400

    ctci

---
+++ CTCI/ArraysAndStrings/IsUnique.py
@@ -0,0 +1,7 @@
+# Implement an algorithm to determine if a string has all unique characters.
+# What if you cannont use any additional data structures?
+
+def IsUnique(string):
+	return True
+
+print(IsUnique("Russell Zheng"))
