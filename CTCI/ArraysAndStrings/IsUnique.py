e631ede3b40d0f87a7e0551053f395c5a45facbd
shoehang <russzheng@gmail.com>
2020-09-16 02:48:36 -0400

    isunique

--- CTCI/ArraysAndStrings/IsUnique.py
+++ CTCI/ArraysAndStrings/IsUnique.py
@@ -1,7 +1,15 @@
 # Implement an algorithm to determine if a string has all unique characters.
 # What if you cannont use any additional data structures?
 
+from BasicHashTable import *
+
 def IsUnique(string):
+	hashTable = HashTable()
+	for character in string:
+		if hashTable.find(character) == character:
+			return False
+		else:
+			hashTable.insert(character, character)
 	return True
 
-print(IsUnique("Russell Zheng"))
+print(IsUnique("abcdefghijjk"))
